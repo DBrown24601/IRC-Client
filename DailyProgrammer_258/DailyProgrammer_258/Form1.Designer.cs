55f08660394cd6967b39d789e521c2f51c8a0db4
Daniel Brown <dbrown24601@gmail.com>
2016-05-21 18:22:00 -0400

    Initial commit
    Initial commit

---
+++ DailyProgrammer_258/DailyProgrammer_258/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DailyProgrammer_258
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.nameBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 464);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(563, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(563, 441);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // nameBox
+            // 
+            this.nameBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.nameBox.Location = new System.Drawing.Point(582, 29);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.ReadOnly = true;
+            this.nameBox.Size = new System.Drawing.Size(138, 424);
+            this.nameBox.TabIndex = 3;
+            this.nameBox.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(594, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Connected Users";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 493);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "IRC Chat (Easy)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox nameBox;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ DailyProgrammer_258/DailyProgrammer_258/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DailyProgrammer_258
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //chat.freenode.net:6667
+            //Nickname : DBrown24601
+            //Username : DBrown24601
+            //Real Name : Dan Brown
+            if (!backgroundWorker1.IsBusy)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+                //Change button to Send/Disconnect
+                button1.Text = "Send";
+            }
+        }
+
+        void writeToBox(string text)
+        {
+            System.Console.WriteLine(text);
+            richTextBox1.AppendText(text + "\n");
+        }
+
+        void writeToNamebox(string name)
+        {
+            nameBox.AppendText(name + "\n");
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+             
+            string host = "chat.freenode.net";
+            int port = 6667;
+            string nick = "DBrown24601";
+            string user = "DBrown24601";
+            string real = "DBrown24601";
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPHostEntry ipHostInfo = Dns.Resolve(host);
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+
+            IPEndPoint ipe = new IPEndPoint(ipAddress, port);
+
+            string recieve = "";
+            string message = "";
+            byte[] bytes = new byte[8192];
+
+            s.Connect(ipe);
+
+            backgroundWorker1.ReportProgress(0,("Connected to " + s.RemoteEndPoint.ToString()));
+
+            message = "NICK " + nick + "\nUSER " + user + " 0 * :" + real + "\nJOIN #reddit-dailyprogrammer\n";
+            s.Send(Encoding.ASCII.GetBytes(message));
+            backgroundWorker1.ReportProgress(0, message);
+            
+            while (true)
+            {
+                recieve = Encoding.ASCII.GetString(bytes, 0, s.Receive(bytes));
+                backgroundWorker1.ReportProgress(0, recieve);
+
+                if (recieve.Contains("353"))
+                {
+                    string[] substrs = recieve.Split(':');
+                    string[] names = substrs[6].Split(' ');
+                    foreach (var name in names)
+                        backgroundWorker1.ReportProgress(1, name);
+                }
+
+                if (recieve.StartsWith("PING"))
+                {
+                    s.Send(Encoding.ASCII.GetBytes(recieve.Replace("PING", "PONG")));
+                    backgroundWorker1.ReportProgress(0, "Pong sent");
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage.ToString() == "0")
+            {
+                writeToBox((string)e.UserState);
+            }
+
+            if (e.ProgressPercentage.ToString() == "1")
+            {
+                writeToNamebox((string)e.UserState);
+            }
+            
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                writeToBox("Canceled!");
+            }
+            else if (e.Error != null)
+            {
+                writeToBox("Error: " + e.Error.Message);
+            }
+            else
+            {
+                writeToBox("Done!");
+            }
+        }
+    }
+}
